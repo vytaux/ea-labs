dd8a61a54b365965fbbf4a5346b23e6b339b169b
Vytautas A <vytautasasm@gmail.com>
2024-03-14 14:19:29 -0500

    changes

--- Lab13/Lab13PartC/src/main/java/com/example/demo/GreetingController.java
+++ Lab13/Lab13PartC/src/main/java/com/example/demo/GreetingController.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
+    Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
     @RequestMapping(value="/greeting")
     public String greeting() {
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
         return "Hello World";
     }
 }
