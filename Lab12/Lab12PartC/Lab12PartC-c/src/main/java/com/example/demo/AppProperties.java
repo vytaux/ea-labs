5c35d6043036377d3a7fbcb2ab3092f731d8b136
Vytautas A <vytautasasm@gmail.com>
2024-03-14 02:19:04 -0500

    Lab12PartC

---
+++ Lab12/Lab12PartC/Lab12PartC-c/src/main/java/com/example/demo/AppProperties.java
@@ -0,0 +1,157 @@
+package com.example.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+
+@ConfigurationProperties(prefix="myapp")
+@Validated
+public class AppProperties {
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String version;
+    private List<String> countries;
+
+    private final Server server = new Server();
+    private final User user = new User();
+
+    @Valid
+    public static class Server {
+        @NotBlank
+        private String url;
+        private String name;
+
+        public Server() {
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Server{" +
+                    "url='" + url + '\'' +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+    public static class User {
+        private String firstname;
+        private String lastname;
+        @NotBlank
+        @Size(min = 8, max = 15)
+        private String username;
+        @NotBlank
+        @Size(min = 8, max = 15)
+        private String password;
+
+        public User() {
+        }
+
+        public String getFirstname() {
+            return firstname;
+        }
+
+        public void setFirstname(String firstname) {
+            this.firstname = firstname;
+        }
+
+        public String getLastname() {
+            return lastname;
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname = lastname;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "firstname='" + firstname + '\'' +
+                    ", lastname='" + lastname + '\'' +
+                    ", username='" + username + '\'' +
+                    ", password='" + password + '\'' +
+                    '}';
+        }
+    }
+
+    public AppProperties() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public List<String> getCountries() {
+        return countries;
+    }
+
+    public void setCountries(List<String> countries) {
+        this.countries = countries;
+    }
+
+    public Server getServer() {
+        return server;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String toString() {
+        return "AppProperties{" +
+                "name='" + name + '\'' +
+                ", version='" + version + '\'' +
+                ", countries=" + countries +
+                ", server=" + server +
+                ", user=" + user +
+                '}';
+    }
+}
---
+++ Lab12/Lab12PartC/Lab12PartC-c/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
+public class DemoApplication implements CommandLineRunner {
+
+	private final AppProperties appProperties;
+
+	public DemoApplication(AppProperties appProperties) {
+		this.appProperties = appProperties;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("AppProperties: " + appProperties);
+	}
+}
